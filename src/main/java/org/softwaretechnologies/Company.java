b0b19bea4d6a970bd0ccc495180ba21a50cf790c
Nikita <nikitatumarev854@gmail.com>
2025-10-09 23:30:01 +0300

    Task 3 company complited

--- src/main/java/org/softwaretechnologies/Company.java
+++ src/main/java/org/softwaretechnologies/Company.java
@@ -22,8 +22,7 @@ public class Company {
      * @param type тип работника
      */
     public void addEmployee(String name, int baseSalary, EmployeeType type) {
-        // TODO: реализуйте вышеуказанную функцию
-
+        employeeList.add(EmployeeFactory.createEmployee(name,baseSalary,type));
     }
 
     /**
@@ -32,9 +31,15 @@ public class Company {
      * @return сумма зарплат всех сотрудников за указанный месяц
      */
     public int getMonthSalary(int month) {
-        // TODO: реализуйте вышеуказанную функцию
+        int totalSalary = 0;
 
-        return 0;
+        for (Employee employee : employeeList) {
+            totalSalary += employee.getMonthSalary(month);
+        }
+
+        return totalSalary;
+
+
     }
 
     public String getName() {
