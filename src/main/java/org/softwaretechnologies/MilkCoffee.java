d738a2d837343e220bbd7018670026c229290f80
Nikita <nikitatumarev854@gmail.com>
2025-10-13 23:14:52 +0300

    task 6 completed

--- src/main/java/org/softwaretechnologies/MilkCoffee.java
+++ src/main/java/org/softwaretechnologies/MilkCoffee.java
@@ -4,7 +4,22 @@ package org.softwaretechnologies;
 //  к стоимости базового напитка добавьте 10.
 //  к описанию добавьте " + milk"
 
-public class MilkCoffee {
+public class MilkCoffee extends Coffee {
+    private CoffeeInterface coffee;
+    public static final int COST = 10;
+
+    public MilkCoffee(CoffeeInterface coffee){
+        this.coffee = coffee;
+    }
+    @Override
+    public int getCost() {
+        return coffee.getCost() + COST;
+    }
+
+    @Override
+    public String description() {
+        return coffee.description() + " + milk";
+    }
 
 
 }
--- src/main/java/org/softwaretechnologies/SugarCoffee.java
+++ src/main/java/org/softwaretechnologies/SugarCoffee.java
@@ -7,6 +7,20 @@ package org.softwaretechnologies;
 //     к описанию добавьте " + sugar"
 //     */
 
-public class SugarCoffee {
+public class SugarCoffee extends Coffee {
+    private CoffeeInterface coffee;
+    public static final int COST = 20;
 
+    public SugarCoffee(CoffeeInterface coffee){
+        this.coffee = coffee;
+    }
+    @Override
+    public int getCost() {
+        return coffee.getCost() + COST;
+    }
+
+    @Override
+    public String description() {
+        return coffee.description() + " + sugar";
+    }
 }
