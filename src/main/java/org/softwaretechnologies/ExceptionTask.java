4e36ae5b77ef469afc07c3db03f361d6c82354fa
Nikita <nikitatumarev854@gmail.com>
2025-10-13 16:06:59 +0300

    Task 5 completed

--- src/main/java/org/softwaretechnologies/ExceptionTask.java
+++ src/main/java/org/softwaretechnologies/ExceptionTask.java
@@ -9,9 +9,11 @@ public class ExceptionTask {
        Вызвана функция printMessage
      */
     public static void printMessage() {
-        throwRuntimeException();
-        // TODO: реализуйте вышеуказанную функцию
-
+        try{
+            throwRuntimeException();
+        } catch (Exception e){
+            System.out.println("Вызвана функция printMessage");;
+        }
     }
 
     /**
@@ -20,8 +22,13 @@ public class ExceptionTask {
      Вызвана функция printMessage2
      */
     public static void printMessage2() throws Exception {
-        throwCatchableException();
-        // TODO: реализуйте вышеуказанную функцию
+        try{
+            throwCatchableException();
+        }catch (Exception e){
+            System.out.println("Вызвана функция printMessage2");
+        }
+
+
     }
 
     private static void throwCatchableException() throws Exception {
@@ -41,7 +48,8 @@ public class ExceptionTask {
      */
     public static int divide(int dividend, int divisor) throws DivideOnNullException {
 
-        // TODO: реализуйте вышеуказанную функцию
+        if(divisor == 0)
+            throw new DivideOnNullException();
         return dividend/divisor;
     }
 
@@ -55,7 +63,14 @@ public class ExceptionTask {
      * @return конкатенацию двух строк: кротчайшую из двух строк с другой строкой.
      */
     public static Optional<String> mergeStrings(String first, String second) {
-        // TODO: реализуйте вышеуказанную функцию
+        if(first == null && second == null)
+            return Optional.empty();
+
+        if(first == null)
+            return Optional.of(second);
+
+        if(second == null)
+            return Optional.of(first);
 
 
 
